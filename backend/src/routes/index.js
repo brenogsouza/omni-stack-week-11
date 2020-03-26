0407fbfa55344648003b030629b17024b49849f5
brenogsouza <breno.souza.developer@gmail.com>
2020-03-25 23:02:17 -0300

    abstraido rotas criando controllers

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,21 +1,10 @@
 const express = require('express')
-const crypto = require('crypto')
-const connection = require('../configs/connection')
+const OngController = require('../controllers/OngController')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (req, res) => {
-  const ongs = await connection('ongs').select('*')
-  return res.json(ongs)
-})
+routes.get('/ongs', OngController.index)
 
-routes.post('/ongs', async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body
-  const id = crypto.randomBytes(4).toString('HEX')
-  await connection('ongs').insert({
-    id, name, email, whatsapp, city, uf
-  })
-  return res.json({ id })
-})
+routes.post('/ongs', OngController.create)
 
 module.exports = routes
