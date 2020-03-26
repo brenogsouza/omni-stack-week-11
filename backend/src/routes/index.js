08f536c0ea0c5e403aff22781fe1f211c6a8535d
brenogsouza <breno.souza.developer@gmail.com>
2020-03-25 22:55:08 -0300

    listado items do banco

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const crypto = require('crypto')
-const connection = require('../database/connection')
+const connection = require('../configs/connection')
 
 const routes = express.Router()
+
+routes.get('/ongs', async (req, res) => {
+  const ongs = await connection('ongs').select('*')
+  return res.json(ongs)
+})
 
 routes.post('/ongs', async (req, res) => {
   const { name, email, whatsapp, city, uf } = req.body
