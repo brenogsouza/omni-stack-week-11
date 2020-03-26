3a523bc3b40bfe53e2655b7003abfe0010484747
brenogsouza <breno.souza.developer@gmail.com>
2020-03-26 13:38:03 -0300

    criado layout page incident

---
+++ frontend/src/pages/profile/Profile.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Logo } from '../../assets';
+import { FiPower, FiTrash2 } from 'react-icons/fi'
+
+import './style.css'
+
+const Profile = () => {
+  return (
+    <div className="profile__container">
+      <header>
+        <img src={Logo} alt="Be To Hero" />
+        <span>Bem Vinda, APAD</span>
+        <Link className="button" to="/incident/new">Cadastrar novo caso</Link>
+        <button type="button">
+          <FiPower size={18} color="#E02141" />
+        </button>
+      </header>
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={16} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={16} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={16} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={16} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+export default Profile;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Login } from './pages/login'
 import { Register } from './pages/register'
+import { Profile } from './pages/profile'
 
 
 const Routes = () => {
@@ -10,6 +11,7 @@ const Routes = () => {
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   )
 }
