2a0fb23fc7e302860e1a8871ac60a3c2944cbf91
brenogsouza <breno.souza.developer@gmail.com>
2020-03-26 00:46:12 -0300

    feito um join no banco para pegar os dados das ongs e tratado os valores

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -9,9 +9,17 @@ module.exports = {
     const [count] = await connection('incidents').count()
 
     const incidents = await connection('incidents')
+      .join('ongs', "ongs.id", "=", "incidents.ong_id")
       .limit(5)
       .offset((page - 1) * 5)
-      .select('*')
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ])
 
     res.header('X-Total-Count', count['count(*)'])
 
@@ -37,13 +45,18 @@ module.exports = {
 
     const ong_id = req.headers.authorization
 
-    const incident = await connection('incidents').where('id', id).select('ong_id').first()
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first()
 
     if (incident.ong_id !== ong_id) {
       return res.status(401).json({ error: 'Operation not permitted' })
     }
 
-    await connection('incidents').where('id', id).delete()
+    await connection('incidents')
+      .where('id', id)
+      .delete()
 
     return res.status(204).send()
 
