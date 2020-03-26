b88a92c2dbef834797e2a93d18160e6a723c2ce9
brenogsouza <breno.souza.developer@gmail.com>
2020-03-25 22:49:09 -0300

    criado ong no banco com post

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('../database/connection')
 
 const routes = express.Router()
 
-routes.post('/users', (req, res) => {
-  return res.json({
-    evento: "teste",
-    aluno: "Breno GS Lopes"
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body
+  const id = crypto.randomBytes(4).toString('HEX')
+  await connection('ongs').insert({
+    id, name, email, whatsapp, city, uf
   })
+  return res.json({ id })
 })
 
 module.exports = routes
