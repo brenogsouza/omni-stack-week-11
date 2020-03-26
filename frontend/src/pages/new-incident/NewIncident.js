ca14a42649aeb3061488029b603e056532f9b2ca
brenogsouza <breno.souza.developer@gmail.com>
2020-03-26 18:58:06 -0300

    finalizado app web cadastrando novo caso

--- frontend/src/pages/new-incident/NewIncident.js
+++ frontend/src/pages/new-incident/NewIncident.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css'
 import { Logo } from '../../assets';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import { API } from '../../services'
+
 const NewIncident = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+
+  const ongId = localStorage.getItem('ong_id')
+  const history = useHistory()
+
+  const handleNewIncident = async (e) => {
+    e.preventDefault()
+
+    const data = {
+      title,
+      description,
+      value
+    }
+
+    try {
+      await API.post('incident', data, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+
+      history.push('/profile')
+
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente.')
+    }
+  }
+
+
   return (
     <div className="new__incident-container ">
       <div className="new__incident-content ">
@@ -18,10 +51,10 @@ const NewIncident = () => {
             Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input type="number" placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input placeholder="Título do caso" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <textarea placeholder="Descrição" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <input type="number" placeholder="Valor em reais" value={value} onChange={(e) => setValue(e.target.value)} />
 
           <button className="button">Cadastrar</button>
         </form>
